9a9db5e8304d0ee8f985fd1744d20725cbe3542b
Mukhathutsheli <charlesndou454@gmail.com>
2024-05-19 18:03:28 +0200

    eish

--- controllers/passport.js
+++ controllers/passport.js
@@ -3,11 +3,10 @@ const session = require('express-session');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const mongoose = require('mongoose');
-const User = require('../models/user.model.js'); 
+const User = require('../models/user.model.js');
+const path = require('path');
 
-
-
-mongoose.connect("mongodb+srv://Qwertyui1:neyon71133@cluster0.zbdv8in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)", {
+mongoose.connect("mongodb+srv://Qwertyui1:neyon71133@cluster0.zbdv8in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
@@ -18,29 +17,24 @@ passport.use(
     {
       callbackURL: "https://playcryptidweb.azurewebsites.net/google/callback",
       clientID: "813500182154-rb9qve19jksavhheklmiiger4kfs52j7.apps.googleusercontent.com",
-      clientSecret:    "GOCSPX-sbXUhLu7FFFbbE6K48kgrZoFLii"
+      clientSecret: "GOCSPX-sbXUhLu7FFFbbE6K48kgrZoFLii"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        // Checking if user already exists in the database
         let user = await User.findOne({ googleId: profile.id });
-       
-    
         if (user) {
-          return done(null, user); // User exists, return user data
+          return done(null, user);
         } else {
-          // create a new user
           user = new User({
             googleId: profile.id,
             displayName: profile.displayName,
             email: profile.emails[0].value
           });
-
-          await user.save(); // Save the new user to the database
-          return done(null, user); // Return the newly created user
+          await user.save();
+          return done(null, user);
         }
       } catch (err) {
-        return done(err, null); // Pass any errors to the done callback
+        return done(err, null);
       }
     }
   )
@@ -63,7 +57,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname,'../public/html')
+  res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 
 app.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
@@ -71,13 +65,13 @@ app.get('/google', passport.authenticate('google', { scope: ['email', 'profile']
 app.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    // Successful authentication, redirect to home page
     res.redirect('/');
   });
 
 app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+  req.logout(() => {
+    res.redirect('/');
+  });
 });
 
 const PORT = process.env.PORT || 5000;
