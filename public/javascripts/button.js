9c02f4db3682f35bb742d3851b4e37102c60b0a0
SOMEONE1703 <siphelel.1703@gmail.com>
2024-05-22 06:13:53 +0200

    invalid jsons

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -1,26 +1,32 @@
 const jsonDataString = sessionStorage.getItem('jsonData');
 var who={};
-var main_obj={};
-let colors=["red", "green", "orange", "blue", "purple"];
+var main_obj = {};
+let colors = ["red", "green", "orange", "blue", "purple"];
 if (jsonDataString) {
-  const jsonData = JSON.parse(jsonDataString);
-    who=jsonData[0];
-    main_obj=jsonData;
-  console.log('Retrieved JSON data from sessionStorage:', jsonData);
+    try {
+        const jsonData = JSON.parse(jsonDataString);
+        who = jsonData[0];
+        main_obj = jsonData;
+        console.log('Retrieved JSON data from sessionStorage:', jsonData);
+
+        var currentNumber = '0';
+        var getText = '0';
+
+        for (let i = 0; i < main_obj[1].rules.length; i++) {
+            let y = document.createElement("div");
+            y.className = "single-clue";
+            y.textContent = main_obj[1].rules[i];
+            y.style.backgroundColor = colors[i];
+            document.getElementById("clue-list").appendChild(y);
+        }
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+        alert("Invalid JSON: " + error.message);
+        window.location.href="/review";
+    }
 } else {
-  console.log('No JSON data found in sessionStorage.');
+    console.log('No JSON data found in sessionStorage.');
 }
-var currentNumber = '0';
-var getText = '0';
-
-for (let i=0;i<main_obj[1].rules.length;i++){
-    let y=document.createElement("div");
-    y.className="single-clue";
-    y.textContent=main_obj[1].rules[i];
-    y.style.backgroundColor=colors[i];
-    document.getElementById("clue-list").appendChild(y);
-}
-
 drawIt();
 
 
